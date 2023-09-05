0e8c5b9df34d69138a7f5c01b095fb32349cf909
Cue <hello@cuebit.dev>
2023-09-05 11:37:05 +0100

    feat(vue): ignore v-bind string escaping

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ module.exports = {
     'vue/custom-event-name-casing': ['error', 'kebab-case'],
     'vue/define-macros-order': 'off',
     'vue/html-closing-bracket-newline': 'off',
+    'vue/no-useless-v-bind': ['error', { ignoreStringEscape: true }],
     'vue/no-v-text-v-html-on-component': 'off',
     'vue/singleline-html-element-content-newline': 'off'
   }
